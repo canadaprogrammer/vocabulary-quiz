866134e7088e1ca7d848e603c38c0f2e31232912
Jin Park <jinpark1504@gmail.com>
2022-04-30 02:32:28 -0400

    save results

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - Multiple choice quiz
 
-- Speed quiz, The page will be changed after 5 seconds.
+- Speed quiz, The page will be changed after 3 seconds.
 
 - Random vocabulary order and the answer choices
 
--- default.js
+++ default.js
@@ -1,3 +1,4 @@
+// Words Data
 const words = {
   word: '단어',
   ship: '배',
@@ -9,46 +10,89 @@ const words = {
   house: '집',
 };
 
-// Choosing a Word for Question
-const keys = Object.keys(words);
-const key_prop = keys[Math.floor(Math.random() * keys.length)];
-const answer = words[key_prop];
-
-// Selecting Answers for Options
+// Remove property
 function withoutProperty(obj, property) {
   const { [property]: unused, ...rest } = obj;
   return rest;
 }
 
-let remain_words = words;
-let options = [answer];
-remain_words = withoutProperty(remain_words, key_prop);
+// Initial set values
+const total_pages = Object.keys(words).length;
+let page_number = 1;
 
-while (options.length < 4) {
-  const remain_keys = Object.keys(remain_words);
-  const selected_key =
-    remain_keys[Math.floor(Math.random() * remain_keys.length)];
-  options.push(words[selected_key]);
-  remain_words = withoutProperty(remain_words, selected_key);
-}
+let unremoved_words = words;
+let key_prop = '';
+let answer = '';
 
-// Shuffle the Options
-// Fisher-Yates shuffle Algorithm
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+// Select Question
+let results = [];
+const choose_word = () => {
+  const keys = Object.keys(unremoved_words);
+  key_prop = keys[Math.floor(Math.random() * keys.length)];
+  answer = unremoved_words[key_prop];
+
+  // Go to result page when it's done
+  if (Object.keys(unremoved_words).length == 0) {
+    print_results();
+    return;
   }
+
+  unremoved_words = withoutProperty(unremoved_words, key_prop);
+
+  // Selecting Answers for Options
+  let remain_words = words;
+  let options = [answer];
+  remain_words = withoutProperty(remain_words, key_prop);
+
+  while (options.length < 4) {
+    const remain_keys = Object.keys(remain_words);
+    const selected_key =
+      remain_keys[Math.floor(Math.random() * remain_keys.length)];
+    options.push(words[selected_key]);
+    remain_words = withoutProperty(remain_words, selected_key);
+  }
+
+  // Shuffle the Options
+  // Fisher-Yates shuffle Algorithm
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  };
+
+  shuffleArray(options);
+
+  let choices = '';
+
+  // Display the Options
+  options.forEach(function (value, index) {
+    choices += '<button class="answers">' + value + '</button>';
+  });
+  document.querySelector(
+    '#page_number'
+  ).innerHTML = `<span>${page_number++} / ${total_pages}</span>`;
+  document.querySelector('#vocabulary').innerHTML = key_prop;
+  document.querySelector('#choices').innerHTML = choices;
+  console.log(document.querySelectorAll('#choices .answers'));
+
+  // Save results
+  // Click Answer
+  document.querySelectorAll('#choices .answers').forEach((e) => {
+    e.addEventListener('click', function (evt) {
+      results.push({
+        question: key_prop,
+        answer: answer,
+        selected: evt.target.innerText,
+        bool: evt.target.innerText == answer,
+      });
+      clearTimeout(timeout);
+      choose_word();
+    });
+  });
 };
 
-shuffleArray(options);
-let choices = '';
-
-// Display the Options
-options.forEach(function (value, index) {
-  choices += '<p>' + value + '</p>';
-});
-document.querySelector('#vocabulary').innerHTML = key_prop;
-document.querySelector('#choices').innerHTML = choices;
+// First question
+choose_word();
